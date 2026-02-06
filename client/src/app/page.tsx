05e061a67b8173b38a146df3b3a1456e79be2872
Zuber <zubermohammed557@gmail.com>
2026-02-06 11:26:55 +0300

    add s

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Button>
-        Click Me
+        Click Me starting!
       </Button>
       <h1 className="text-3xl font-bold text-center mt-10">Welcome to the Home Page</h1>
     </div>
