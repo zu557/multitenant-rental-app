974694f756a8129d61f994299f2339c064d62b31
Zuber <zubermohammed557@gmail.com>
2026-02-06 11:23:51 +0300

    added a button component to the home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
+      <Button>
+        Click Me
+      </Button>
       <h1 className="text-3xl font-bold text-center mt-10">Welcome to the Home Page</h1>
     </div>
   );
