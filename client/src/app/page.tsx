417aa4fa0925bf65708aff6e9ae8990e9e636709
Zuber <zubermohammed557@gmail.com>
2026-02-06 11:19:44 +0300

    first commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center mt-10">Welcome to the Home Page</h1>
+    </div>
+  );
+}
